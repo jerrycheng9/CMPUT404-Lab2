53a7e38cb1b6aa4dd9871014a09f3a986c085318
jcheng1 <jcheng1@ualberta.ca>
2016-01-12 09:25:48 -0700

    Lab 2 stuff

---
+++ ServerDemo.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import socket, os
+
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+serverSocket.bind(("0.0.0.0", 12346))
+
+serverSocket.listen(5)
+
+while True:
+	(incomingSocket, address) = serverSocket.accept()
+
+	childPid = os.fork()
+	if (childPid != 0):
+		#we must be still in the connection accepting process
+		continue
+	#we must in a client talking process
+	outgoingSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	outgoingSocket.connect(("www.google.com", 80))
+	done = False
+	while not done:
+		#fix cpu use with poll() or select()
+		incomingSocket.setblocking(0)
+		try:
+			part = incomingSocket.recv(2048)
+		except IOError, exception:
+			if exception.errno == 11:
+				part = None
+			else:
+				raise
+		if (part):
+			outgoingSocket.sendall(part)
+		outgoingSocket.setblocking(0)
+		try:
+			part = outgoingSocket.recv(2048)
+		except IOError, exception:
+			if exception.errno == 11:
+				part = None
+			else:
+				raise
+		if (part):
+			incomingSocket.sendall(part)
